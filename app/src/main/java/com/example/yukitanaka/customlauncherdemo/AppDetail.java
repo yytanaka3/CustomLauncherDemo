b2f017a435f123c73e1387ca0a5a2cb6450034ce
Yuki Tanaka <sim.univ.tanaka@gmail.com>
2017-08-11 15:50:32 +0900

    ゆうし シンプルUI first commit
    参考
    https://blog.areare.net/archives/8441

---
+++ app/src/main/java/com/example/yukitanaka/customlauncherdemo/AppDetail.java
@@ -0,0 +1,13 @@
+package com.example.yukitanaka.customlauncherdemo;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by YukiTanaka on 2017/08/11.
+ */
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
